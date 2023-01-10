9e3609b4c4362e81aec2e119961714b5d08665da
Olha-frontend <morozova19921@icloud.com>
2023-01-10 19:17:26 +0200

    add API

---
+++ src/index.js
@@ -0,0 +1,16 @@
+function searchLocation(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = "a33b693cfbefd271b0ed075f9a8f65f0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let locationButton = document.querySelector("#current-loc");
+locationButton.addEventListener("click", getCurrentLocation);
