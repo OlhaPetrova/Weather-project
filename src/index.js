fca809f25bb5e818153282709d196bd6c98dc7bd
Olha-frontend <morozova19921@icloud.com>
2023-01-18 19:39:40 +0200

    add forecast

--- src/index.js
+++ src/index.js
@@ -29,6 +29,35 @@ function formatDay(timestamp) {
 
   return days[day];
 }
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
 
 function showCity(event) {
   event.preventDefault();
@@ -52,7 +81,7 @@ function displayWeatherCondition(response) {
   let iconElement = document.querySelector("#icon");
   let dateElement = document.querySelector("#date");
 
-windSpeed.innerHTML  = response.data.wind.speed;
+  windSpeed.innerHTML = response.data.wind.speed;
   pressure.innerHTML = Math.round(response.data.main.pressure);
   newCity.innerHTML = response.data.name;
   celsiusTemperature = response.data.main.temp;
@@ -107,3 +136,5 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+displayForecast();
