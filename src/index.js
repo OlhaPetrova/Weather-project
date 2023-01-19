0adaf4f48c972243ec2003e246662fac03c8ec0c
Olha-frontend <morozova19921@icloud.com>
2023-01-19 20:03:14 +0200

    add forecast API

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ function formatDay(timestamp) {
 
   return days[day];
 }
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
@@ -57,6 +57,11 @@ function displayForecast() {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
   console.log(forecastHTML);
+}
+function getForecast(coordinates) {
+  let apiKey = "a33b693cfbefd271b0ed075f9a8f65f0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function showCity(event) {
